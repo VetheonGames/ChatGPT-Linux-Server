d46d7473677634e1258718ce862a8504d056b15d
VetheonGames <ceo@pixelatedstudios.net>
2023-01-25 18:19:47 -0700

    Put the code in a class, add some handlers for errors or if the file isn't found and change the way we are parsing the config

--- chatgpt.rb
+++ chatgpt.rb
@@ -4,21 +4,37 @@ require 'sinatra'
 require 'json'
 require 'openai'
 
-config = JSON.parse(File.read('config.json'))
+class Server
+  def initialize
+    begin
+      @config = JSON.parse(File.read('config.json'))
+    rescue Errno::ENOENT => e
+      puts "Error: config.json file not found, please check that the file exists in the current directory: #{e.class}"
+      exit
+    rescue JSON::ParserError => e
+      puts "Error: config.json file is not valid JSON, please check the format of the file: #{e.class}"
+      exit
+    end
 
-set :bind, config['server']['ip']
-set :port, config['server']['port']
+    set :bind, @config['server']['ip']
+    set :port, @config['server']['port']
+  end
 
-get '/query' do
-  query = params[:query]
-  api_key = params[:api_key]
-  response = OpenAI::Completion.create(
-    engine: 'text-davinci-002',
-    prompt: query,
-    max_tokens: 2048,
-    api_key: api_key
-  )
-  response.choices.first.text
+  def run
+    get '/query' do
+      api_key = params[:api_key]
+      return { status: 'error', message: 'Invalid API key' }.to_json if api_key != @config['api_key']
+
+      query = params[:query]
+      response = OpenAI::Completion.create(
+        engine: 'text-davinci-002',
+        prompt: query,
+        max_tokens: 2048,
+        api_key: api_key
+      )
+      { status: 'ok', data: response.choices.first.text }.to_json
+    end
+
+    run!
+  end
 end
-
-run!
